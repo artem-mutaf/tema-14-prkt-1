96ce273f1d9dcab868f81093f7252c7da44ae106
artem-mutaf <123882882+artem-mutaf@users.noreply.github.com>
2024-01-22 09:17:43 +0200

    Add files via upload

---
+++ T14prkt1/Point.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T14prkt1
+{
+    class Point
+    {
+        double x;
+
+        double y;
+        static int count;
+
+        
+
+        public Point(double x,double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Point()
+        {
+            x = 1.0;
+            y = 1.0;
+        }
+        static Point()
+        {
+            int count = 0;
+        }
+        public double GetX
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public double GetY
+        {
+            get{ return y; }
+            set { y = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{x}\n{y}\n{count}";
+        }
+    }
+}
---
+++ T14prkt1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T14prkt1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Введите координаты 1 точки x,y");
+            Point points = new Point();
+            points.GetX = double.Parse(Console.ReadLine());
+            points.GetY = double.Parse(Console.ReadLine());
+            //Console.WriteLine(points.ToString());
+            Console.WriteLine($"Kоординаты 2 точки x,y");
+            Point points1 = new Point();
+            points1.GetX = 1.0;
+            points.GetY = 1.0;
+            Console.WriteLine(points1.ToString());
+            Console.ReadKey();
+            
+            
+        }
+    }
+}
